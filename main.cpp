c8192417eaaf47b1104963495318129cb19d467a
hakimaro <hakimaro@users.noreply.github.com>
2023-02-05 15:23:48 +0300

    Add examples
    Добавил примеры работы программы

--- main.cpp
+++ main.cpp
@@ -5,32 +5,36 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QVector<qreal> x1;
+    QVector<qreal> y1;
+    QVector<qreal> x2;
+    QVector<qreal> y2;
+    QVector<qreal> x3;
+    QVector<qreal> y3;
+
+    QVector<QString> legend;
+    legend.append("sin(x)");
+    legend.append("cos(x)");
+    legend.append("sin(x)+cos(x)");
+
+    for(qreal i = -5; i <= 5; i+=0.1) {
+        x1.append(i);
+        y1.append(sin(i));
+        x2.append(i);
+        y2.append(cos(i));
+        x3.append(i);
+        y3.append(sin(i)+cos(i));
+    }
 
     QVector<QVector<qreal>> x;
     QVector<QVector<qreal>> y;
 
-    QVector<qreal> x1;
-    QVector<qreal> y1;
-    QVector<QString> legend;
-    legend.append("y=x^2");
-    legend.append("y=4x");
-    for(int i = -10; i <= 10; i++) {
-        x1.append(i);
-        y1.append(i*i);
-    }
-
-    QVector<qreal> x2;
-    QVector<qreal> y2;
-
-    for(int i = -15; i <= 15; i++) {
-        x2.append(i);
-        y2.append(4*i);
-    }
-
     x.append(x1);
     x.append(x2);
+    x.append(x3);
     y.append(y1);
     y.append(y2);
+    y.append(y3);
 
     // Запуск
     FunctionWindow w(x, y, "y", "x", legend);
